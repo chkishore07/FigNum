1589880b8e1c0c7bf6cb982355bee4d2ac584ee8
chkishore07 <46424102+chkishore07@users.noreply.github.com>
2021-10-13 22:30:24 +0100

    Update WeatherForecastController.cs

--- FigNumAPI/Controllers/WeatherForecastController.cs
+++ FigNumAPI/Controllers/WeatherForecastController.cs
@@ -10,8 +10,7 @@ namespace FigNumAPI.Controllers
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
-    {
-    
+    {    
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
